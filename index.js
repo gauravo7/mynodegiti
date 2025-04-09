45d0141096e747cf35a51898834489ca091d5a34
gauravo7 <gaurav@o7services.com>
2025-04-09 09:59:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World Test')
+  res.send('This si Home Page for App Service')
+})
+
+app.get('/about', (req, res) => {
+  res.send('<h2>Welome to About Page</h2>')
 })
 
 app.listen(8080,function(){
